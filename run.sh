4e42af236011feaa2f2e3a93b7233870e327ce74
anlongfei <1056432250@qq.com>
2019-09-12 14:38:13 +0800

    modify

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# ====================================================
+#   Copyright (C)2019 All rights reserved.
+#
+#   Author        : anlongfei
+#   Email         : 在 vimrc 文件中添加 let g:file_copyright_email = 'your email'
+#   File Name     : run.sh
+#   Last Modified : 2019-09-12 14:36
+#   Describe      :
+#
+# ====================================================
+
+git add .
+git commit -m"modify"
+git push origin master
